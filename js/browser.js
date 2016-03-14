25f26c5cc6ca1e43a45d3e49c556fb6786e3b456
Rob King and Schuyler Klaassen <schuyler.m.k@gmail.com>
2016-03-14 16:21:51 -0700

    Add Gulp functions for dev or prod.builds

--- js/browser.js
+++ js/browser.js
@@ -1,9 +1,6 @@
 //interface
 var Journal = require('./journal.js').Journal;
 var Entry = require('./journal.js').Entry;
-// var moment = require('moment');
-
-
 
 $(document).ready(function() {
   $('#journal').submit(function(event){
@@ -11,7 +8,6 @@ $(document).ready(function() {
     var title = $('#title').val();
     var entry = $('#entry').val();
     var newEntry = new Entry(title, entry);
-    // newEntry.date = moment().format('MMMM Do YYYY, h:mm:ss a');
     $('#wordCounts').append("<li>Journal entry: '" + newEntry.title + "' made on " + newEntry.date + " has " + newEntry.wordCount() + " words.</li>");
   });
 });
